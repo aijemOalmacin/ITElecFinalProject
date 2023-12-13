c89c9f716de1c7b8e2b59f9ebf5238d67e6658cd
Aijem <aijemalmacin2018@gmail.com>
2023-12-13 19:08:15 +0800

    Creating backend

--- src/Login.js
+++ src/Login.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios'
 
 function Login(){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    function handSubmit(event){
+        event.preventDefault();
+        axios.post('http://localhost:8081/login', {email, password })
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+    }
     return(
         <div className='d-flex vh-100 justify-content-center align-items-center bg-primary'>
             <div className='p-3 bg-white w-25'>
-                <form action=''>
+                <form onSubmit={handSubmit}>
                     <div className='mb-3'>
                         <label htmlFor='email'>email</label>
-                        <input type='email' placeholder='Enter email' className='form-control'/>
+                        <input type='email' placeholder='Enter email' className='form-control'
+                        onChange={e => setEmail(e.target.value)}/>
                     </div>
                     <div className='mb-3'>
                         <label htmlFor='password'>password</label>
-                        <input type='password' placeholder='Enter password' className='form-control'/>
+                        <input type='password' placeholder='Enter password' className='form-control'
+                        onChange={e => setPassword(e.target.value)}/>
                     </div>
                     <button className='btn btn-success'>Login</button>
                 </form>
