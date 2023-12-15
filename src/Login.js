93efe19000e69f27e755f4f35739a2d62b8e419d
Aijem <aijemalmacin2018@gmail.com>
2023-12-15 15:22:41 +0800

    Makin css for Login

--- src/Login.js
+++ src/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom'; // Updated import
+import './Login.css';
 
 function Login() {
   const [email, setEmail] = useState('');
@@ -21,23 +22,32 @@ function Login() {
   }
 
     return(
-        <div className='d-flex vh-100 justify-content-center align-items-center bg-primary'>
-            <div className='p-3 bg-white w-25'>
-                <form onSubmit={handSubmit}>
-                    <div className='mb-3'>
-                        <label htmlFor='email'>email</label>
-                        <input type='email' placeholder='Enter email' className='form-control'
-                        onChange={e => setEmail(e.target.value)}/>
-                    </div>
-                    <div className='mb-3'>
-                        <label htmlFor='password'>password</label>
-                        <input type='password' placeholder='Enter password' className='form-control'
-                        onChange={e => setPassword(e.target.value)}/>
-                    </div>
-                    <button className='btn btn-success'>Login</button>
-                </form>
+        <div className="d-flex-container">
+        <div className="p-3 login-container">
+          <form onSubmit={handSubmit}>
+            <div className="mb-3">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                placeholder="Enter email"
+                className="form-control"
+                onChange={e => setEmail(e.target.value)}
+              />
             </div>
+            <div className="mb-3">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                placeholder="Enter password"
+                className="form-control"
+                onChange={e => setPassword(e.target.value)}
+              />
+            </div>
+            <button className="btn btn-success">Login</button>
+          </form>
         </div>
+      </div>
+  
     )
 
 }
